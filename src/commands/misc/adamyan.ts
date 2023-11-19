f425410dbea6979f55ce368aa294c1f2dcb1b153
hopfeeeen <noahh4j@gmail.com>
2023-11-19 22:14:20 +0100

    fix

---
+++ src/commands/misc/adamyan.ts
@@ -0,0 +1,33 @@
+import BaseCommand from "@structures/BaseCommand";
+import BaseClient from "@structures/BaseClient";
+import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
+
+export default class NevarCommand extends BaseCommand {
+    public constructor(client: BaseClient) {
+        super(client, {
+            name: "adamyan",
+            description: "A real model?",
+            localizedDescriptions: {
+                de: "Ein echter Model? Hier ist er"
+            },
+            cooldown: 1000,
+            dirname: __dirname,
+            slashCommand: {
+                addCommand: true,
+                data: new SlashCommandBuilder()
+            }
+        });
+    }
+
+    public async dispatch(interaction: any, data: any): Promise<void> {
+        this.interaction = interaction;
+        this.guild = interaction.guild;
+        await this.geilerBot();
+    }
+
+    private async geilerBot(): Promise<any> {
+        const nevarEmbed: EmbedBuilder = this.client.createEmbed("### Model??!?! MODEL!!!", null, "normal");
+nevarEmbed.setImage("https://cdn.discordapp.com/attachments/1116797977432961197/1175905401464684715/image.png?ex=656cee3e&is=655a793e&hm=aa21cd0fc3ebd1f8ff2dc08ab3947dc5121178ef8c6cc12eb3eddaff21d3681b&");
+return this.interaction.followUp({ embeds: [nevarEmbed] });
+    }
+}
