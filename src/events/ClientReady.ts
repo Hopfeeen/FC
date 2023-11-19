566f1bef7278bcecdb9d251b0f4158c4c89e47b5
hopfeeeen <noahh4j@gmail.com>
2023-11-19 21:34:43 +0100

    fix

--- src/events/ClientReady.ts
+++ src/events/ClientReady.ts
@@ -51,15 +51,15 @@ export default class {
 				nevarEmbed = this.client.createEmbed("Ein depremierender Tag für unseren Server! Heute wurden leider nur **{0} Messages** geschrieben. Dabei waren **{1} Menschen** beteiligt. Danke für eure Aktivität <3", null, "normal", count, writers);
 				nevarEmbed.setImage("https://cdn.discordapp.com/attachments/1116797977432961197/1175891378211926066/HectorMessages0-100.png?ex=656ce12f&is=655a6c2f&hm=f9f22670e79a5b7181c74326ba12bf82653f0092be42e4f107145ea23173b796&");
 			}
-			if (count > 500 < 100){
+			if (count > 100 && count < 500){
 				nevarEmbed = this.client.createEmbed("Hey, morgen wirds besser! Heute wurden **{0} Messages** geschrieben. Dabei haben **{1} Menschen** geschrieben. Danke für eure Aktivität <3 ", null, "normal", count, writers);
 				nevarEmbed.setImage("https://cdn.discordapp.com/attachments/1116797977432961197/1175892447000281179/8cfb1468-4d89-4af2-9657-77295ce53c32.png?ex=656ce22e&is=655a6d2e&hm=56dabc7036a13db119a378ff06cb22f6b75741642364b1bb734cb1741f2cdb4a&")
 			}
-			if (count > 1000 < 500){
+			if (count < 1000 && count > 500){
 				nevarEmbed = this.client.createEmbed("Klasse! Heute wurden **{0} Messages** von **{1} Personen** geschrieben! Danke für eure Aktivität <3", null, "normal", count, writers);
 				nevarEmbed.setImage("https://cdn.discordapp.com/attachments/1116797977432961197/1175893445932499065/image-fotor-20231119211945.png?ex=656ce31c&is=655a6e1c&hm=0b60780e61cbbe8a053e4a483a5b8d869f94090231fc247196e0b3e2cacc5789&")
 			}
-			if (count > 1500 < 1000){
+			if (count < 1500 && count > 1000){
 				nevarEmbed = this.client.createEmbed("Sehr Stark!!! Alleine heute wurden **{0} Messages** geschrieben. Dabei haben **{1} Leute** geholfen! Vielen Dank für eure Aktivität <3", null, "normal", count, writers);
 				nevarEmbed.setImage("https://cdn.discordapp.com/attachments/1116797977432961197/1175894350564180038/team.png?ex=656ce3f4&is=655a6ef4&hm=6269796f6c039a58475ce50a4507ccea15a5e7f99aca2c3f769113e8a204535f&")
 			}
