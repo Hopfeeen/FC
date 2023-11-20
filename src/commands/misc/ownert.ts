b2837407c19c7ab9d66ada7024b6e9d188229fe1
hopfeeeen <noahh4j@gmail.com>
2023-11-21 00:07:22 +0100

    fix

---
+++ src/commands/misc/ownert.ts
@@ -0,0 +1,32 @@
+import BaseCommand from "@structures/BaseCommand";
+import BaseClient from "@structures/BaseClient";
+import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
+
+export default class NevarCommand extends BaseCommand {
+    public constructor(client: BaseClient) {
+        super(client, {
+            name: "owner",
+            description: "Owner",
+            localizedDescriptions: {
+                de: "Owner"
+            },
+            cooldown: 1000,
+            dirname: __dirname,
+            slashCommand: {
+                addCommand: true,
+                data: new SlashCommandBuilder()
+            }
+        });
+    }
+
+    public async dispatch(interaction: any, data: any): Promise<void> {
+        this.interaction = interaction;
+        this.guild = interaction.guild;
+        await this.geilerBot();
+    }
+
+    private async geilerBot(): Promise<any> {
+        const nevarEmbed: EmbedBuilder = this.client.createEmbed("### Der Owner auf dem Server ist Hopfen", null, "normal")
+return this.interaction.followUp({ embeds: [nevarEmbed] });
+    }
+}
