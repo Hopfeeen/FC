cd8f8b8571763d87e850c574d8cf3a5f2f754481
Jonas <65913879+1887jonas@users.noreply.github.com>
2023-12-03 18:11:28 +0100

    improvement: messages

--- src/commands/misc/messages.ts
+++ src/commands/misc/messages.ts
@@ -3,7 +3,7 @@ import BaseClient from "@structures/BaseClient";
 import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import * as fs from "fs"
 
-export default class NevarCommand extends BaseCommand {
+export default class MessagesCommand extends BaseCommand {
     public constructor(client: BaseClient) {
         super(client, {
             name: "messages",
@@ -23,16 +23,16 @@ export default class NevarCommand extends BaseCommand {
     public async dispatch(interaction: any, data: any): Promise<void> {
         this.interaction = interaction;
         this.guild = interaction.guild;
-        await this.geilerBot();
+        await this.messages();
     }
 
-    private async geilerBot(): Promise<any> {
+    private async messages(): Promise<any> {
         const messagesFile: any = JSON.parse(fs.readFileSync("./assets/messages.json"));
         const count: number = messagesFile?.count || 0;
         const writers: number = messagesFile?.writer.length || 0;
         const mostActiveUser = Object.keys(messagesFile.users).reduce((a, b) => messagesFile.users[a] > messagesFile.users[b] ? a : b);
-		const mostActiveUserMessages = messagesFile.users[mostActiveUser];
-        const nevarEmbed: EmbedBuilder = this.client.createEmbed("Heute wurden bisher **{0} Nachrichten** von **{1} Menschen** geschrieben! Bisher ist <@{2}> der Aktivste! Danke für eure Aktivität <3", "arrow", "normal", count, writers, mostActiveUser);
-return this.interaction.followUp({ embeds: [nevarEmbed] });
+	const mostActiveUserMessages = messagesFile.users[mostActiveUser];
+        const messagesEmbed: EmbedBuilder = this.client.createEmbed("Heute wurden bisher **{0} Nachrichten** von **{1} Menschen** geschrieben! Bisher ist <@{2}> der Aktivste! Danke für eure Aktivität <3", "arrow", "normal", count, writers, mostActiveUser);
+	return this.interaction.followUp({ embeds: [messagesEmbed] });
     }
 }
