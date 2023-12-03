05a971c56831c11450a6a7e0e835ec223cfcb7db
Jonas <65913879+1887jonas@users.noreply.github.com>
2023-12-03 18:15:59 +0100

    improvement: sexy

--- src/commands/misc/sexy.ts
+++ src/commands/misc/sexy.ts
@@ -2,7 +2,7 @@ import BaseCommand from "@structures/BaseCommand";
 import BaseClient from "@structures/BaseClient";
 import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
 
-export default class NevarCommand extends BaseCommand {
+export default class SexyCommand extends BaseCommand {
     public constructor(client: BaseClient) {
         super(client, {
             name: "sexy",
@@ -22,12 +22,12 @@ export default class NevarCommand extends BaseCommand {
     public async dispatch(interaction: any, data: any): Promise<void> {
         this.interaction = interaction;
         this.guild = interaction.guild;
-        await this.geilerBot();
+        await this.sexy();
     }
 
-    private async geilerBot(): Promise<any> {
-        const nevarEmbed: EmbedBuilder = this.client.createEmbed("### Sexy Person is here...", null, "normal");
-nevarEmbed.setImage("https://cdn.discordapp.com/attachments/1116797977432961197/1176558345918881873/SexyHector.png?ex=656f4e59&is=655cd959&hm=b4c2db1d2a187c67b8659bbf364c2c00412c41a5cc2b46e424a0b332d8ab1b45&");
-return this.interaction.followUp({ embeds: [nevarEmbed] });
+    private async sexy(): Promise<any> {
+        const sexyEmbed: EmbedBuilder = this.client.createEmbed("### Sexy Person is here...", null, "normal");
+        sexyEmbed.setImage("https://cdn.discordapp.com/attachments/1116797977432961197/1176558345918881873/SexyHector.png?ex=656f4e59&is=655cd959&hm=b4c2db1d2a187c67b8659bbf364c2c00412c41a5cc2b46e424a0b332d8ab1b45");
+        return this.interaction.followUp({ embeds: [sexyEmbed] });
     }
 }
