06309c4551fffcce4fb0ed77617f9009fdc541c2
Jonas <65913879+1887jonas@users.noreply.github.com>
2024-01-15 15:07:50 +0100

    Aktualisieren von 8ball.ts

--- src/commands/fun/8ball.ts
+++ src/commands/fun/8ball.ts
@@ -2,13 +2,13 @@ import BaseCommand from "@structures/BaseCommand";
 import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import BaseClient from "@structures/BaseClient";
 
-export default class EightballCommand extends BaseCommand {
+export default class AskCommand extends BaseCommand {
 	public constructor(client: BaseClient) {
 		super(client, {
-			name: "8ball",
-			description: "Ask a question and get magical answers",
+			name: "ask",
+			description: "Ask a question and get 100% correct answers",
 			localizedDescriptions: {
-				de: "Stelle eine Frage und erhalte magische Antworten"
+				de: "Stelle eine Frage und erhalte 100% wahre Antworten",
 			},
 			cooldown: 1000,
 			dirname: __dirname,
@@ -19,33 +19,28 @@ export default class EightballCommand extends BaseCommand {
 						.setRequired(true)
 						.setName("question")
 						.setNameLocalizations({
-							de: "frage"
+							de: "frage",
 						})
 						.setDescription("Enter your question")
 						.setDescriptionLocalizations({
-							de: "Gib deine Frage ein"
-						})
-				)
-			}
+							de: "Gib deine Frage ein",
+						}),
+				),
+			},
 		});
 	}
 
 	public async dispatch(interaction: any, data: any): Promise<void> {
 		this.interaction = interaction;
 		this.guild = interaction.guild;
-		return this.getAnswer();
+		return this.getAnswer(this.interaction.options.getString("question"));
 	}
 
-	private async getAnswer(): Promise<void> {
-		const eightBallAnswers: string[] = this.translate("answers");
-		const randomAnswer: string =
-			eightBallAnswers[Math.floor(Math.random() * eightBallAnswers.length)];
-		const eightBallEmbed: EmbedBuilder = this.client.createEmbed(
-			"{0}",
-			"question",
-			"normal",
-			randomAnswer
-		);
+	private async getAnswer(question: string): Promise<any> {
+		const eightBallAnswers: any = this.translate("answers");
+		const randomAnswer: string = eightBallAnswers[Math.floor(Math.random() * eightBallAnswers.length)];
+  const text: string = this.client.emotes.question + " " + question + "\n\n" + this.client.emotes.arrow + " " + randomAnswer;
+		const eightBallEmbed: EmbedBuilder = this.client.createEmbed(text, null, "normal");
 		return this.interaction.followUp({ embeds: [eightBallEmbed] });
 	}
 }
